037a7139e591996b908ff55c08a25176ec80e170
AdornedJoker <walterbrothersgaming@gmail.com>
2025-12-22 12:39:22 -0500

    Some Suggestions

--- Client/Network/Server.cpp
+++ Client/Network/Server.cpp
@@ -10,43 +10,25 @@ namespace RBX
 		{
 			SocketDescriptor socketDescriptor(port, "");
 			if (!this->rakPeer.get()->Startup(32, threadSleepTime, &socketDescriptor, 1))
-			{
-				std::string error("Failed to start network server");
-				throw std::runtime_error(error);
-			}
+				throw std::runtime_error("Failed to start network server");
+
 			this->outgoingPort = port;
 
 			StandardOut::singleton()->print(MESSAGE_INFO, "Starting network server on port %d", port);
-
-			unsigned NumberOfAddresses = this->rakPeer->GetNumberOfAddresses();
-
 			StandardOut::singleton()->print(MESSAGE_INFO, "IP addresses:");
 
-			unsigned int index = 0;
-			if(NumberOfAddresses > 0)
-			{
-				do
-				{
-					StandardOut::singleton()->print(MESSAGE_INFO, "%s", this->rakPeer->GetLocalIP(index));
-					index++;
-				}
-				while (index < NumberOfAddresses);
-			}
+			for(unsigned int i = 0; i < this->rakPeer->GetNumberOfAddresses(); i++)
+				StandardOut::singleton()->print(MESSAGE_INFO, "%s", this->rakPeer->GetLocalIP(i));
+
 			Peer::updateNetworkSimulator();
 		}
 
 		void Server::stop(int blockDuration)
 		{
 			if(this->rakPeer.get()->IsActive())
-			{
 				this->rakPeer.get()->Shutdown(blockDuration);
-			}
-			Instance::removeAllChildren();
-		}
 
-		bool Server::askAddChild(Instance const *instance) const
-		{
-			return dynamic_cast<ClientProxy*>(const_cast<Instance*>(instance)) != NULL;
+			Instance::removeAllChildren();
 		}
 	}
 }
